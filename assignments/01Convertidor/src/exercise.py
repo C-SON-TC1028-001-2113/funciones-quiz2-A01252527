a1fd001ba0d00335202c62048d419950448b296d
A01252527 <a01252527@itesm.mx>
2021-08-27 17:14:03 +0000

    examen

--- assignments/01Convertidor/src/exercise.py
+++ assignments/01Convertidor/src/exercise.py
@@ -1,7 +1,33 @@
 # Escribe tus funciones abajo de esta línea
-
+def pies_cm(pies): 
+    feet = (pies*30.48)
+    return(feet)
+def pulgadas_cm(pulgadas):
+    inches = (pulgadas*2.54)
+    return (inches)
+def yardas_cm(yardas): 
+    yards = (yardas*91.44)
+    return(yards)
 def main():
-    # Escribe tu código abajo de esta línea
+    print('1. pies a cm, 2. pulgadas a cm, 3. yardas a cm' ) 
+    opciones = str(input('Introduce una opcion: '))
+    cantidad = int(input('Introduce la cantidad: '))
+    if cantidad > 0: 
+        if opciones == '1': 
+            feet = pies_cm(cantidad)
+            print(feet)
+        elif opciones == '2': 
+            inches = pulgadas_cm(cantidad)
+            print(inches)
+        elif opciones == '3':
+            yards = yardas_cm(cantidad)
+            print(yards)
+        else:
+            print('Error')
+    else: 
+        print('Error')
+        
+
 
 if __name__ == '__main__':
     main()
